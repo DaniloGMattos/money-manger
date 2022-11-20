9cc6628df679dd251e22f69e360a05022a508b2e
Danilo Mattos <mattosgdanilo@gmail.com>
2022-11-20 09:45:09 -0300

    feat: custom summary hook

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -3,6 +3,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { priceFormatter } from "../utils/formatters";
+import { useSummary } from "../hooks/useSummary";
 const SummaryContainer = styled.section`
   max-width: 1120px;
   width: 100%;
@@ -41,20 +42,7 @@ const SummaryCard = styled.div<SummaryCardProps>`
 `;
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc;
-    },
-    { income: 0, outcome: 0, total: 0 }
-  );
+  const summary = useSummary();
   return (
     <SummaryContainer>
       <SummaryCard>
